eb6d0e06c049abe9ac1bfb99cdf66f3f76df4372
agray998 <a.gray.4797@gmail.com>
2023-10-02 12:41:44 +0100

    added api source

---
+++ calculator-api/calc.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"log"
+)
+
+type calc_req struct {
+	Mode int
+	BeforeVat, AfterVat, VatRate float64
+}
+
+func calculator(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var calc, result calc_req
+	err := decoder.Decode(&calc)
+
+    if err != nil {
+        panic(err)
+    }
+
+	if calc.Mode == 1 {
+		result.BeforeVat = calc.BeforeVat
+		result.VatRate = calc.VatRate
+		result.AfterVat = calc.BeforeVat + (calc.BeforeVat * (calc.VatRate / 100))
+	}
+	if calc.Mode == 2 {
+		result.AfterVat = calc.AfterVat
+		result.VatRate = calc.VatRate
+		result.BeforeVat = calc.AfterVat / (1 + (calc.VatRate / 100))
+	}
+	if calc.Mode == 3 {
+		result.AfterVat = calc.AfterVat
+		result.BeforeVat = calc.BeforeVat
+		result.VatRate = (calc.AfterVat - calc.BeforeVat) * (100 / calc.BeforeVat)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(&result)
+}
+
+func main() {
+	http.HandleFunc("/calc", calculator)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
